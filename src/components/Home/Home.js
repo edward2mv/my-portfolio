bc0abffa6c8bca6b822e2dfeb3b694030d72edce
edward2mv <edward.emv@gmail.com>
2021-12-11 15:08:08 -0400

    ....

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -19,7 +19,7 @@ function Home() {
 
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name"> EDWARD MEDINA</strong>
+                <strong className="main-name"> EUDYS MEDINA</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
