0c783722d2fba50c5d789ba2f036af7e8062c545
edward2mv <edward.emv@gmail.com>
2022-02-17 21:30:32 -0400

    change projects broken links

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -28,7 +28,7 @@ function Projects() {
               isBlog={false}
               title="Cryptocurrency Finance Price Tracker App Using Hooks and Axios"
               description="This Frontend web application using React and the cryptocurrency API called Coingecko, in addition to the Bootstrap framework. This project was born as an idea, in order to practice with a simple project and also understand some APIs from the world of cryptocurrencies."
-              link="https://github.com/edward2mv/react-coingecko-api"
+              link="#"
             />
           </Col>
 
@@ -36,9 +36,9 @@ function Projects() {
             <ProjectCard
               imgPath={leaf}
               isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              link="https://github.com/soumyajit4419/Plant_AI"
+              title="PlatziPunksNFT Marketplace DApp"
+              description="PlatziPunks is an NFT marketplace created from data stored entirely on the blockchain and displayed through an application deployed on a decentralized file system."
+              link="#"
             />
           </Col>
 
@@ -46,9 +46,9 @@ function Projects() {
             <ProjectCard
               imgPath={editor}
               isBlog={false}
-              title="Editor.io"
+              title="edme.tech personal site & portfolio"
               description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              link="https://github.com/soumyajit4419/Editor.io"
+              link="#"
             />
           </Col>
 
@@ -58,7 +58,7 @@ function Projects() {
               isBlog={false}
               title="Bits-0f-C0de"
               description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              link="https://github.com/soumyajit4419/Bits-0f-C0de"
+              link="#"
             />
           </Col>
 
@@ -68,7 +68,7 @@ function Projects() {
               isBlog={false}
               title="Ai For Social Good"
               description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              link="https://github.com/soumyajit4419/AI_For_Social_Good"
+              link="#"
             />
           </Col>
 
@@ -79,7 +79,7 @@ function Projects() {
               title="Face Recognition and Emotion Detection"
               description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
               Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              link="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
+              link="#"
             />
           </Col>
         </Row>
