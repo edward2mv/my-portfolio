5e1041eb16292b278ef3a1cba3a23a8b2abbb893
edward2mv <edward.emv@gmail.com>
2022-03-17 14:55:20 -0400

    projects description changes

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,12 +3,12 @@ import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.jpeg";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
-import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+import coingator from "../../Assets/Projects/coingator.png";
+import ava from "../../Assets/Projects/avapunks.jpg";
+import portfolio from "../../Assets/Projects/porfolio.png";
+import udenfy from "../../Assets/Projects/udenfy.PNG";
+import rollingstone from "../../Assets/Projects/rollingstone.png";
+import simona from "../../Assets/Projects/simona.png";
 
 function Projects() {
   return (
@@ -24,96 +24,112 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={coingator}
               isBlog={false}
-              title="CoinGator"
-              description=" Cryptocurrency Finance Price Tracker App Using Hooks and Axios. This Frontend web application using React and the cryptocurrency API called Coingecko, in addition to the Bootstrap framework. This project was born as an idea, in order to practice with a simple project and also understand some APIs from the world of cryptocurrencies."
+              title="CoinGator React Native App"
+              description=" Cryptocurrency Finance Price Tracker App Using Hooks and Axios. This Frontend web application using React and the cryptocurrency API called Coingecko, in addition to the Bootstrap framework. This project was born as an idea, in order to practice with a simple project and also understand some APIs from the world of cryptocurrencies.
+              This project was built using these technologies:
+
+React Native,
+Node.js,
+Express.js,
+CSS3,
+VsCode,
+Vercel."
               link="#"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={leaf}
+              imgPath={ava}
               isBlog={false}
               title="Avatar Punks DApp"
-              description="AvatarPunks is an NFT marketplace created from data stored entirely on the blockchain and displayed through an application deployed on a decentralized file system."
+              description="AvatarPunks is an NFT marketplace created from data stored entirely on the blockchain and displayed through an application deployed on a decentralized file system.
+              This project was built using these technologies:
+
+React.js,
+Node.js,
+Express.js,
+CSS3,
+VsCode,
+Vercel."
               link="#"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath={portfolio}
               isBlog={false}
               title="My Personal site & portfolio"
               description="My personal portfolio edme.tech which features some of my github projects as well as my resume and technical skills.
 
 This project was built using these technologies:
 
-React.js
-Node.js
-Express.js
-CSS3
-VsCode
-Vercel"
-              link="#"
+React.js,
+Node.js,
+Express.js,
+CSS3,
+VsCode,
+Vercel."
+              link="http://edme.tech/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={udenfy}
               isBlog={false}
               title="Udenfy, Inc."
               description="Udenfy, Inc. is a marketing agency and training platform specializing in e-commerce.
               This project was built using these technologies:
 
-WordPress
-Elementor
-Gatsby.js
-React.js
-backbone.js
-MySQL
+WordPress,
+Elementor,
+Gatsby.js,
+React.js,
+backbone.js,
+MySQL,
 PHP.
 "
-              link="#"
+              link="https://udenfy.com/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={suicide}
+              imgPath={rollingstone}
               isBlog={false}
               title="Hotel Club Rolling Stone"
               description="Clubrollingstone.com , is a family-run hotel located in Boca Chica.
               This project was built using these technologies:
 
-WordPress
-Vue.js
-CSS3
-MySQL
+WordPress,
+Vue.js,
+CSS3,
+MySQL,
 PHP.
 "
-              link="#"
+              link="https://clubrollingstone.com/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={emotion}
+              imgPath={simona}
               isBlog={false}
-              title="Simona beauty salon"
+              title="Salón Simona"
               description="Salonsimona.com , is a WordPress site.
               This project was built using these technologies:
 
-WordPress
-Elementor
-CSS3
-MySQL
+WordPress,
+Elementor,
+CSS3,
+MySQL,
 PHP.
 "
-              link="#"
+              link="http://salonsimona.com/"
             />
           </Col>
         </Row>
